f42716b38cca90b7abb86a854012b806e4148170
Raphael Jacob <r.jacob2002@gmail.com>
2017-06-03 17:41:43 +0200

    Add Function to generate X1 Packages

--- fttxpy/tx_serial.py
+++ fttxpy/tx_serial.py
@@ -14,7 +14,7 @@ class TXSerial():
         self.ser = serial.Serial(self.dev)
         self.SerialLock = threading.Lock()
 
-        self.X1TID = 0  # will be counted up for each package sent by the PC
+        self.X1TID = 1  # will be counted up for each package sent by the PC
         self.X1SID = 0  # will start at 0 and then set by TX-C
 
     def close(self):
@@ -60,21 +60,66 @@ class TXSerial():
 
     # the internal X.1 data structure looks like this
     # all other data will be calculated as needed / TID/SID will be counted aumatically
-    __exampleX1 = {
-        "from": 0,
-        "to": 0,
-        "CC": 0,
-        "TA":
-            {
-                0: "bytes",
-                2: "bytes"
-            },
+    _exampleX1 = {
+        "from": 2,
+        "to": 1,
+        "CC": 1,
+        #"TA":
+        #    {
+        #        0: bytearray([0, 1, 2]),
+        #        2: bytearray([3, 4, 5])
+        #    },
+        "TA": {},
         "CRCok": True  # only present for recieved packages
     }
 
     def sendX1Package(self, data):
         """
         """
+        PackageStart = bytearray([0x02, 0x55])
+        PackageEnd = bytearray([0x03])
+        PackageHeaderLen = 20
+        try:
+            TALen = len(data["TA"][list(data["TA"].keys())[0]]) * len(data["TA"])
+        except IndexError:
+            TALen = 0
+        PackageLen = (PackageHeaderLen + TALen).to_bytes(2, byteorder='big')
+        PackageFrom = data["from"].to_bytes(4, byteorder='little')
+        PackageTo = data["to"].to_bytes(4, byteorder='little')
+        PackageFromTo = PackageFrom + PackageTo
+        PackageTID = self.X1TID.to_bytes(2, byteorder='little')
+        PackageSID = self.X1SID.to_bytes(2, byteorder='little')
+        PackageTIDSID = PackageTID + PackageSID
+        PackageCC = data["CC"].to_bytes(4, byteorder='little')
+        PackageTAs = len(data["TA"]).to_bytes(4, byteorder='little')
+        PackageHeader = PackageLen + PackageFromTo + PackageTIDSID + PackageCC + PackageTAs
+
+        PackageData = bytearray([])
+        PackageHeaderData = PackageHeader + PackageData
+
+        Chacksum = 0
+        for b in PackageHeaderData:
+            Chacksum -= b
+        PackageChecksum = Chacksum.to_bytes(2, byteorder='big', signed=True)
+        PackageFooter = PackageChecksum + PackageEnd
+
+        PackageHeaderData = PackageHeaderData + PackageStart
+        FULLPackage = PackageHeaderData + PackageFooter
+
+        i = 0
+        while True:
+            exit = False
+            array = []
+            for x in range(4):
+                try:
+                    array.append(FULLPackage[i + x])
+                except:
+                    exit = True
+            print(str(i) + ":" + " ".join("%02x" % b for b in array))
+            i += 4
+            if exit:
+                break
+
         return(True)
 
     def reciveX1Package(self):
