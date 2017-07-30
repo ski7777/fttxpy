93e01e1e482dbbe4c795a8eef15223249bc7e2a4
Raphael Jacob <r.jacob2002@gmail.com>
2017-07-30 12:45:05 +0200

    New test.py

--- test.py
+++ test.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 #
 from fttxpy import *
+import json
 TX = fttxpy()
-
 print("-----")
-input("Wait oninput!")
+input()
+TX.Data[0]["Output"]["Duty"][0] = 512
+input()
 print("-----END-----")
+print(json.dumps(TX.Data, indent=4, sort_keys=True))
