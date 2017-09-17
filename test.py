9156311d1a5748d17084c61737d79769c747b3ef
Raphael Jacob <r.jacob2002@gmail.com>
2017-09-17 21:15:44 +0200

    Change the test program

--- test.py
+++ test.py
@@ -1,12 +1,33 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-from fttxpy import *
 import json
+import time
+
+from fttxpy import *
+
 TX = fttxpy()
-print("-----")
-input()
-TX.Data[0]["Output"]["Duty"][0] = 512
-input()
-print("-----END-----")
-print(json.dumps(TX.Data, indent=4, sort_keys=True))
+while True:
+    TX.Data[0]["Output"]["Duty"][1] = 0
+    TX.Data[1]["Output"]["Duty"][1] = 0
+    for x in range(1, 513):
+        TX.Data[0]["Output"]["Duty"][0] = x
+        TX.Data[1]["Output"]["Duty"][0] = x
+        time.sleep(0.005 + 0.005 / 512 * x)
+    time.sleep(0.2)
+    for x in reversed(range(0, 512)):
+        TX.Data[0]["Output"]["Duty"][0] = x
+        TX.Data[1]["Output"]["Duty"][0] = x
+        time.sleep(0.005 + 0.005 / 512 * x)
+    TX.Data[0]["Output"]["Duty"][0] = 0
+    TX.Data[1]["Output"]["Duty"][0] = 0
+    for x in range(1, 513):
+        TX.Data[0]["Output"]["Duty"][1] = x
+        TX.Data[1]["Output"]["Duty"][1] = x
+        time.sleep(0.005 + 0.005 / 512 * x)
+    time.sleep(0.2)
+    for x in reversed(range(0, 512)):
+        TX.Data[0]["Output"]["Duty"][1] = x
+        TX.Data[1]["Output"]["Duty"][1] = x
+        time.sleep(0.005 + 0.005 / 512 * x)
+#print(json.dumps(TX.Data, indent=4, sort_keys=True))
