cfd0a1b5fddd0118b7c2aaced57a1eb02b2a49a8
Raphael Jacob <r.jacob2002@gmail.com>
2017-09-20 12:44:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='fttxpy',
       author='Raphael Jacob',
       author_email='r.jacob2002@gmail.com',
       url='https://github.com/ski7777/fttxpy',
-      license='GPLv3'
+      license='GPLv3',
+      packages=['fttxpy']
       )
